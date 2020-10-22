29da411943990183115d8ced6f6b32107bb2a455
Just-D-A <prospeedcuber@gmail.com>
2020-10-22 23:13:59 +0300

    add lib

--- app/src/main/java/com/example/omegalint/MainActivity.kt
+++ app/src/main/java/com/example/omegalint/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.omegalint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val l = "This string has more 130 symbols ...................................................................................."
 }
