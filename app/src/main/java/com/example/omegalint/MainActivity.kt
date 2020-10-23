a9f9620f4c9014b05f137dba18c333a44ed3197e
just-d-a <prospeedcuber@gmail.com>
2020-10-23 15:23:29 +0300

    change gradle version

--- app/src/main/java/com/example/omegalint/MainActivity.kt
+++ app/src/main/java/com/example/omegalint/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.omegalint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Main : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    val l = "This string has more 130 symbols ...................................................................................."
+    val l = "This string has more 130 symbols ......................................................................................"
 }
