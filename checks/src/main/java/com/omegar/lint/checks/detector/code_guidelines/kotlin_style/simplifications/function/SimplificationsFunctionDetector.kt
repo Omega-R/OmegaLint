9db4668e69518c50ac240924a6690627dd537b92
just-d-a <prospeedcuber@gmail.com>
2020-11-23 16:11:46 +0300

    debug

--- checks/src/main/java/com/omegar/lint/checks/detector/code_guidelines/kotlin_style/simplifications/function/SimplificationsFunctionDetector.kt
+++ checks/src/main/java/com/omegar/lint/checks/detector/code_guidelines/kotlin_style/simplifications/function/SimplificationsFunctionDetector.kt
@@ -44,7 +44,7 @@ class SimplificationsFunctionDetector : Detector(), Detector.UastScanner {
 						ISSUE,
 						node,
 						context.getNameLocation(node),
-						ISSUE.getExplanation(TextFormat.TEXT)
+						text + ISSUE.getExplanation(TextFormat.TEXT)
 					)
 				}
 			}
