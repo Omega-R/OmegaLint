f5a37b59003300f5fd0f74d45884828deed6a82c
just-d-a <prospeedcuber@gmail.com>
2020-11-30 15:38:00 +0300

    change MaxFunctionsArgumentsDetector for constructors

--- checks/src/main/java/com/omegar/lint/checks/detector/code_guidelines/kotlin_style/restrictions/params_count/MaxFunctionsArgumentsDetector.kt
+++ checks/src/main/java/com/omegar/lint/checks/detector/code_guidelines/kotlin_style/restrictions/params_count/MaxFunctionsArgumentsDetector.kt
@@ -10,20 +10,20 @@ class MaxFunctionsArgumentsDetector : Detector(), Detector.UastScanner {
 		/** Issue describing the problem and pointing to the detector implementation */
 		@JvmField
 		val ISSUE: Issue = Issue.create(
-            id = "OMEGA_NOT_EXCEED_MAX_ARGUMENTS_COUNT_IN_FUNCTION",
-            briefDescription = "Arguments count does not match the coding convention. Function body should not be empty.",
-            explanation = """
+			id = "OMEGA_NOT_EXCEED_MAX_ARGUMENTS_COUNT_IN_FUNCTION",
+			briefDescription = "Arguments count does not match the coding convention. Function body should not be empty.",
+			explanation = """
                   Method has too much arguments. In functions, the number of parameters must not exceed 5.
                   http://wiki.omega-r.club/dev-android-code#rec228191623
                     """,
-            category = Category.CORRECTNESS,
-            priority = 7,
-            severity = Severity.WARNING,
-            implementation = Implementation(
-                MaxFunctionsArgumentsDetector::class.java,
-                Scope.JAVA_FILE_SCOPE
-            )
-        )
+			category = Category.CORRECTNESS,
+			priority = 7,
+			severity = Severity.WARNING,
+			implementation = Implementation(
+				MaxFunctionsArgumentsDetector::class.java,
+				Scope.JAVA_FILE_SCOPE
+			)
+		)
 
 		private const val MAX_COUNT_OF_ARGUMENTS = 5
 	}
@@ -35,14 +35,18 @@ class MaxFunctionsArgumentsDetector : Detector(), Detector.UastScanner {
 	override fun createUastHandler(context: JavaContext): UElementHandler? {
 		return object : UElementHandler() {
 			override fun visitMethod(node: UMethod) {
+				if (node.isConstructor) {
+					return
+				}
+
 				val params = node.uastParameters
 				if (params.size > MAX_COUNT_OF_ARGUMENTS) {
 					context.report(
-                        ISSUE,
-                        node,
-                        context.getNameLocation(node),
-                        ISSUE.getExplanation(TextFormat.TEXT)
-                    )
+						ISSUE,
+						node,
+						context.getNameLocation(node),
+						ISSUE.getExplanation(TextFormat.TEXT)
+					)
 				}
 			}
 		}
