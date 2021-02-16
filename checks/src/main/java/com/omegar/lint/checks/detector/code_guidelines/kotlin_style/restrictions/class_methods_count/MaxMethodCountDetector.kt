5adf4e61c28134e91e54b283c5490d55bee2697b
just-d-a <prospeedcuber@gmail.com>
2021-02-16 17:38:30 +0300

    change MaxMethodCountDetector report message

--- checks/src/main/java/com/omegar/lint/checks/detector/code_guidelines/kotlin_style/restrictions/class_methods_count/MaxMethodCountDetector.kt
+++ checks/src/main/java/com/omegar/lint/checks/detector/code_guidelines/kotlin_style/restrictions/class_methods_count/MaxMethodCountDetector.kt
@@ -37,12 +37,12 @@ class MaxMethodCountDetector : Detector(), Detector.UastScanner {
 			override fun visitClass(node: UClass) {
 				val resultMethods = mutableListOf<UMethod>()
 				node.methods.forEach {
-					if(!it.isVarArgs && !it.isConstructor) {
+					if (!it.isVarArgs && !it.isConstructor) {
 						resultMethods.add(it)
 					}
 				}
 				if (resultMethods.size > MAX_METHOD_COUNT) {
-					context.report(ISSUE, node, context.getNameLocation(node), /*ISSUE.getExplanation(TextFormat.TEXT)*/resultMethods.size.toString())
+					context.report(ISSUE, node, context.getNameLocation(node), ISSUE.getExplanation(TextFormat.TEXT))
 				}
 			}
 		}
